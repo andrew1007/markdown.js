6e1c82203501aba6e4370d17003d26c39743c6b4
andrew <yueh.andrew@gmail.com>
2021-08-31 22:04:31 -0700

    remove takor files

--- src/utils.ts
+++
@@ -1,5 +0,0 @@
-import { ShapeOfMatchers } from './types'
-
-export function getPojoSize(obj: ShapeOfMatchers) {
-    return Object.keys(obj as any).length
-}
--- src/utils/preflight.ts
+++ src/utils/preflight.ts
@@ -1,8 +1,5 @@
-import testWriter from './testWriter';
-
 (() => {
     let missingFileCount = 0
-    missingFileCount += testWriter()
     if (missingFileCount > 0) {
         throw new Error(
             `${missingFileCount} files were written. That means you haven't written a necessary file yet`
